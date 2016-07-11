afb842e79da3b39ae53106c25db640aeb619004e
Erik C. Olson <erikcaineolson@gmail.com>
2016-07-10 21:04:27 -0700

    Displays containers at the end of the script.
    Provides credit where credit is due.

--- delete-containers.sh
+++ delete-containers.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-###############################################
-###                                         ###
-###   Basic script to delete one or more    ###
-###   Docker containers.                    ###
-###                                         ###
-###   Usage:                                ###
-###      ./delete-containers.sh all|${id}   ###
-###                                         ###
-###############################################
+#######################################################################
+###                                                                 ###
+###   Basic script to delete one or more                            ###
+###   Docker containers.                                            ###
+###                                                                 ###
+###   Based on @ngpestelos fantastic answer:                        ###
+###       https://gist.github.com/ngpestelos/4fc2e31e19f86b9cf10b   ###
+###   Usage:                                                        ###
+###      ./delete-containers.sh all|${id}                           ###
+###                                                                 ###
+#######################################################################
 
 
 if [ $# -eq 0 ]
@@ -30,3 +32,6 @@ else
     # assume this is an ID and delete only the container noted
     docker rm $1
 fi
+
+# display remaining containers
+docker ps -a
